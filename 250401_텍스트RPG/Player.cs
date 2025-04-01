e996c70d5b24244463114fa21cfaabfe827fd3ab
2000yhlee <backtoback0320@gmail.com>
2025-04-01 14:13:08 +0900

    Create Player.cs

---
+++ 250401_텍스트RPG/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250401_텍스트RPG
+{
+    class Player
+    {
+        private int power;
+        public int Power { get { return power; } set { power = value; } }
+    }
+}
